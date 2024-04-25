8f3d2ea2617c88e98ea5793ad59e8ad429a3db29
南秋SouthAki <xieleihan@gmail.com>
2024-04-26 00:18:06 +0800

    Add one demoFile

---
+++ pages/index/index.js
@@ -0,0 +1,26 @@
+// index.js
+Page({
+  // 页面初始数据,
+  data:{
+    list:[]
+  },
+  // onLoad 生命周期函数,监听页面在加载的时候加载里面的内容
+  onLoad(options){
+    // 微信有一个专门发起网络请求的方法 wx.request
+    wx.request({
+      // url填入接口api的地址
+      url: 'https://apis.netstart.cn/zhihudaily/stories/latest',
+      // method 是请求的方法 这里的需要根据需要填入是 POST 或者是 GET
+      method:"GET",
+      // 接口请求成功后需要有一个回调函数
+      success:(res)=>{
+        // success成功执行
+        console.log("请求成功");
+        // 将服务器返回的数据保存在页面的AppData中
+        this.setData({
+          list:res.data
+        })
+      }
+    })
+  }
+})
